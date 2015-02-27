0763be92bf349fb6df6196e1c538872b38ae3440
Spencer Schoeben <spencer@netspencer.com>
2015-02-27 01:46:00 -0500

    starting point yo

---
+++ lib/twitter/cleanse.rb
@@ -0,0 +1,7 @@
+require "twitter/cleanse/version"
+
+module Twitter
+  module Cleanse
+    # Your code goes here...
+  end
+end
---
+++ lib/twitter/cleanse/version.rb
@@ -0,0 +1,5 @@
+module Twitter
+  module Cleanse
+    VERSION = "0.0.1"
+  end
+end
